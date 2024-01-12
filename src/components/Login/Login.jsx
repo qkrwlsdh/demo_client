bae6f5d4cb285f47ae70503cbe2f7757cc64d5c7
박진오 <jopark@bankedin.io>
2024-01-12 12:21:13 +0900

    구글 OTP 정상인증 여부 확인 수정

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,7 @@
-import {React, useState, useEffect} from "react";
+import {React, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import ReactModal from "react-modal";
-
 
 const Login = (props) => {
     const [loginId, setLoginId] = useState("");
@@ -12,14 +11,7 @@ const Login = (props) => {
     const [qrModalIsOpen, setQrModalIsOpen] = useState(false);
     const [responseData, setResponseData] = useState({});
     const [qrUrl, setQrUrl] = useState(null);
-    const [qrModalIsClosed, setQrModalIsClosed] = useState(false);
     let navigate = useNavigate();
-
-    // useEffect(() => {
-    //   console.log('useEffect()');
-    //   window.location.reload();
-    // }, [qrModalIsOpen]);
-
 
     const resetInput = () => {
         setLoginId("");
@@ -45,11 +37,8 @@ const Login = (props) => {
         return true;
     }
 
-
     const loginBtnClick = async (e) => {
         const requestData = { loginId, loginPw };
-
-        console.log();
 
         if (!validations()) {
             return;
@@ -60,12 +49,10 @@ const Login = (props) => {
                     , requestData
                     , {headers: {'Content-Type': 'application/json'},
                 });
-                console.log('loginBtnClick/response: ', response);
 
                 // 구글 OTP 등록
                 if (response.status === 200) {
                     setResponseData({loginId: response.data.loginId, googleOtp: response.data.googleOtp});
-                    console.log('loginBtnClick/response.data: ', response.data);
                     setModalIsOpen(true);
 
                 // 구글 OTP 미등록
@@ -74,18 +61,10 @@ const Login = (props) => {
 
                     // QR 등록 모달 오픈
                     setQrModalIsOpen(true);
-                    console.log('QR 모달 오픈')
 
                 } else if (response.status === 204) {
                     alert('로그인 5회 이상 실패 시 로그인이 불가합니다.');
-
                 }
-
-                // if (qrModalIsClosed === true) {
-                //     setResponseData({loginId: response.data.loginId, googleOtp: response.data.googleOtp});
-                //     setModalIsOpen(true);
-                //     console.log('OTP 모달 오픈', responseData);
-                // }
             } catch (err) {
                 alert('로그인 실패');
                 console.log('Login/loginBtnClick/err: ', err);
@@ -105,31 +84,37 @@ const Login = (props) => {
         e.preventDefault();
         setValue(e.target.value);
     };
-    // submit 이벤트
-    const handleSubmit = (e) => {
+    // otp code submit 이벤트
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const data = {
-            "token": value,
-            "loginId": responseData.loginId,
-            "googleOtp": responseData.googleOtp
-        }
-
-        axios.post('/api/auth', data).then(res => {
-            console.log(res)
-            console.log(res.data.verify)
-            if (res.data.verify) {
+        await afterAuth().then(() => {
             navigate("/mypage", { state: { loginId: responseData.loginId }});
-            } else {
+        }).catch(() => {
             alert('인증 코드가 맞지 않습니다.');
-            }
         });
     };
 
-    const qrModalClose = () => {
-        setQrModalIsOpen(false)
-        setQrModalIsClosed(true);
-    };
+    function afterAuth() {
+        return new Promise((resolve, reject) => {
+            const data = {
+                "token": value,
+                "loginId": responseData.loginId,
+                "googleOtp": responseData.googleOtp
+            }
+            axios.post('/api/auth', data).then(res => {
+                console.log(res.data);
+                console.log(res.status);
 
+                if (res.data.verify === 'true') {
+                    console.log('isValid: ', res.data.verify);
+                    resolve();
+                } else {
+                    console.log('isValid: ', res.data.verify);
+                    reject();
+                }
+            });
+        });
+    }
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -144,7 +129,7 @@ const Login = (props) => {
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID</label>
-                                <input type="text" name="userid" id="userid" value={loginId} onChange={(e) => setLoginId(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="test@bankedin.io" required="" />
+                                <input type="text" name="userid" id="userid" autoFocus value={loginId} onChange={(e) => setLoginId(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="test@bankedin.io" required="" />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -167,7 +152,7 @@ const Login = (props) => {
                 </div>
             </div>
             <ReactModal className="relative" isOpen={modalIsOpen} ariaHideApp={false}>
-                <form className="" action="#"  onSubmit={handleSubmit}>
+                <form className="">
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-2/4 my-6 mx-auto max-w-3xl">
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -180,7 +165,7 @@ const Login = (props) => {
                                     </button>
                                 </div>
                                 <input className="m-5 p-3 border" type="number" name="code" autoFocus onChange={handleInput} placeholder="6자리 입력" />
-                                <button className="m-5 bg-primary-600 hover:bg-primary-700 text-white font-bold py-2 px-4 border-blue-700 rounded">확인</button>
+                                <input type="submit" className="m-5 bg-primary-600 hover:bg-primary-700 text-white font-bold py-2 px-4 border-blue-700 rounded" onClick={handleSubmit} value="확인" />
                             </div>
                         </div>
                     </div>
@@ -192,7 +177,7 @@ const Login = (props) => {
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                                     <h3 className="text-3xl font-semibold mt-1">QR CODE 등록</h3>
-                                    <button className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={qrModalClose}>
+                                    <button className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={() => qrModalIsOpen(false)}>
                                         <span className="bg-transparent text-black h-6 w-6 block outline-none focus:outline-none">
                                         ×
                                         </span>
