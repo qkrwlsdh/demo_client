495f722d4e8ce17d184449e6df39948e8a75149c
박진오 <jopark@bankedin.io>
2024-05-17 11:11:37 +0900

    물적현황보고 분리

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import FormLayout_PW from './pages/login/FormLayout_PW';
 import FormLayout_Reissue from './pages/login/FormLayout_Reissue';
 import UnNotifyDataHandling from './pages/backoffice/UnNotifyDataHandling'
 import KeyinSmartroPaymentData from './pages/backoffice/KeyinSmartroPaymentData'
-import AnimaTemplate from './pages/ppt-template/AnimaTemplate';
 
 /**
  * @BrowserRouter는 페이지를 새로고침하지 않고도 주소를 변경할 수 있도록 한다.
@@ -41,7 +40,6 @@ function App() {
           <Route path="/formlayout_reissue" element={<FormLayout_Reissue />} />
           <Route path="/resend-unnotify" element={<UnNotifyDataHandling/>} />
           <Route path="/keyin-smartro" element={<KeyinSmartroPaymentData/>} />
-          <Route path="/ppt1" element={<AnimaTemplate/>} />
 
 
         </Routes>
--- src/pages/ppt-template/Finhub.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Finhub = ({ className }) => {
-  return (
-    <div
-      className={`w-[24px] h-[38px]bg-[url(https://c.animaapp.com/lvQ7FD7L/img/finhub-1-2@2x.png)]  bg-cover bg-[50%_50%] ${className}`}
-    />
-  );
-};
